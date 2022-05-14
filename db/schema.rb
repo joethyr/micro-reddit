76918467651f4dabe6d91e0964d74c20faa82da4
joethyr <joethayr@gmail.com>
2022-05-13 22:48:38 -0600

    fix Post column migration

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,7 @@ ActiveRecord::Schema.define(version: 2022_05_14_041716) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.string "string"
-    t.string "body"
-    t.string "text"
+    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
