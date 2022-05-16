6a55fd8c04541001d22a33c4f31a875cb2919c84
joethyr <joethayr@gmail.com>
2022-05-16 11:19:57 -0600

    migrate foreign key userid to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  belongs_to :user
+
   validates :title, presence: true, length: { maximum: 60 }, uniqueness: true
   validates :body, presence: true, length: { minimum: 15 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { maximum: 24 }, uniqueness: true
   validates :password, presence: true, length: { in: 6..25 }
   validates :email, presence: true, uniqueness: true
+
+  has_many :posts
 end
